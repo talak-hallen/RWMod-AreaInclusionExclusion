5335d39fbbbd7d1357d61ccc19f0c328ceed3cac
talakhallen <79050176+talakhallen@users.noreply.github.com>
2021-02-14 22:47:17 -0500

    Update author
    Update LICENSE author.
    Add link to source code of the original mod.
    Remove PublishedFileId.txt so the mod can be published again.
    Update AssemblyInfo.cs copyright and GUID.

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AreaInclusionExclusion")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("9ce004c2-604d-406a-abf5-59a56f50dae1")]
+[assembly: Guid("ef4a2c1f-bec9-488d-a879-d239d2ebf116")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
